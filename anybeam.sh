e93c9be6dc9c1c81702fcca892ea7d146c5cff08
ChristopherRush <chris.uclan@gmail.com>
2020-06-16 17:13:27 +0100

    update

--- anybeam.sh
+++ anybeam.sh
@@ -8,7 +8,7 @@ function mode_select()
   echo "Mode 3 - RGB 666 + GPIO26"
   echo "Mode 4 - RGB 666 + GPOI26 + GPIO27"
   while true; do
-          read -p "Option: " mode
+          read -p "Option: " mode < /dev/tty
           if [[ "$mode" == 1 ]] || [[ "$mode" == 2 ]] || [[ "$mode" == 3 ]] || [[ "$mode" == 4 ]]; then
                   break
           fi
@@ -97,7 +97,7 @@ EOF
 
 function reboot()
 {
-  echo "Rebooting system now......."
+  echo "Rebooting system now......." < /dev/tty
   sleep 3
   reboot
   exit 0
