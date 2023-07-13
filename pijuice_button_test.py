59905c7b1dbcd8d7924b46db26409de024f3d42c
Christopher Rush <chris.uclan@gmail.com>
2023-07-13 20:01:13 +0100

    Update pijuice_button_test.py

--- pijuice_button_test.py
+++ pijuice_button_test.py
@@ -1,28 +1,32 @@
 from pijuice import PiJuice
+import time
 
 # Initialize PiJuice interface
 pijuice = PiJuice(1, 0x14)
 
 while True:
     # Get the status of the buttons
-    button_status = pijuice.status.GetButtonStatus()
+    button_events = pijuice.status.GetButtonEvents()
 
-    # Function to check button status
-    def check_button_status(button_name):
-        status = button_status['data'][button_name]
-        if status == 'SINGLE_PRESS':
-            print(f"{button_name} button single press")
-        elif status == 'DOUBLE_PRESS':
-            print(f"{button_name} button double press")
-        elif status == 'LONG_PRESS':
-            print(f"{button_name} button long press")
-        else:
-            print(f"{button_name} button is not pressed")
+    if button_events['error'] == 'NO_ERROR':
+        button_data = button_events['data']
 
-    # Check status of each button
-    check_button_status('SW1')
-    check_button_status('SW2')
-    check_button_status('SW3')
+        def check_button_status(button_name):
+            status = button_data[button_name]
+            if status['SINGLE_PRESS']:
+                print(f"{button_name} button single press")
+                pijuice.status.SetButtonEvent(button_name, 'SINGLE_PRESS', False)  # Reset the event
+            if status['DOUBLE_PRESS']:
+                print(f"{button_name} button double press")
+                pijuice.status.SetButtonEvent(button_name, 'DOUBLE_PRESS', False)  # Reset the event
+            if status['LONG_PRESS']:
+                print(f"{button_name} button long press")
+                pijuice.status.SetButtonEvent(button_name, 'LONG_PRESS', False)  # Reset the event
+
+        # Check status of each button
+        check_button_status('SW1')
+        check_button_status('SW2')
+        check_button_status('SW3')
 
     # Delay to prevent CPU overuse
     time.sleep(0.1)
