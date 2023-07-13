625bafd456c0386859a852558b5df325ae73d838
Christopher Rush <chris.uclan@gmail.com>
2023-07-13 19:15:55 +0100

    Create pijuice_button_test.py

---
+++ pijuice_button_test.py
@@ -0,0 +1,25 @@
+from pijuice import PiJuice
+
+# Initialize PiJuice interface
+pijuice = PiJuice(1, 0x14)
+
+while True:
+    # Get the status of the buttons
+    button_status = pijuice.status.GetButtonStatus()
+
+    # Function to check button status
+    def check_button_status(button_name):
+        status = button_status['data'][button_name]
+        if status == 'SINGLE_PRESS':
+            print(f"{button_name} button single press")
+        elif status == 'DOUBLE_PRESS':
+            print(f"{button_name} button double press")
+        elif status == 'LONG_PRESS':
+            print(f"{button_name} button long press")
+        else:
+            print(f"{button_name} button is not pressed")
+
+    # Check status of each button
+    check_button_status('SW1')
+    check_button_status('SW2')
+    check_button_status('SW3')
