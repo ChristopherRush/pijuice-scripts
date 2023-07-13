20a5a1441c1a8f6317dd4c9d0475d09ac76adf80
Christopher Rush <chris.uclan@gmail.com>
2023-07-13 19:39:14 +0100

    Update pijuice_button_test.py

--- pijuice_button_test.py
+++ pijuice_button_test.py
@@ -4,6 +4,18 @@ import time
 # Initialize PiJuice interface
 pijuice = PiJuice(1, 0x14)
 
+def check_button_status(button_name, button_data):
+    status = button_data[button_name]
+    if status['SINGLE_PRESS']:
+        print(f"{button_name} button single press")
+        pijuice.status.SetButtonEvent(button_name, 'SINGLE_PRESS', False)  # Reset the event
+    if status['DOUBLE_PRESS']:
+        print(f"{button_name} button double press")
+        pijuice.status.SetButtonEvent(button_name, 'DOUBLE_PRESS', False)  # Reset the event
+    if status['LONG_PRESS']:
+        print(f"{button_name} button long press")
+        pijuice.status.SetButtonEvent(button_name, 'LONG_PRESS', False)  # Reset the event
+
 while True:
     # Get the status of the buttons
     button_events = pijuice.status.GetButtonEvents()
@@ -11,22 +23,10 @@ while True:
     if button_events['error'] == 'NO_ERROR':
         button_data = button_events['data']
 
-            def check_button_status(button_name):
-            status = button_data[button_name]
-            if status['SINGLE_PRESS']:
-                print(f"{button_name} button single press")
-                pijuice.status.SetButtonEvent(button_name, 'SINGLE_PRESS', False)  # Reset the event
-            if status['DOUBLE_PRESS']:
-                print(f"{button_name} button double press")
-                pijuice.status.SetButtonEvent(button_name, 'DOUBLE_PRESS', False)  # Reset the event
-            if status['LONG_PRESS']:
-                print(f"{button_name} button long press")
-                pijuice.status.SetButtonEvent(button_name, 'LONG_PRESS', False)  # Reset the event
-
         # Check status of each button
-        check_button_status('SW1')
-        check_button_status('SW2')
-        check_button_status('SW3')
+        check_button_status('SW1', button_data)
+        check_button_status('SW2', button_data)
+        check_button_status('SW3', button_data)
 
     # Delay to prevent CPU overuse
     time.sleep(0.1)
