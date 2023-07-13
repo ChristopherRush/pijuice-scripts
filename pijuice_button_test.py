075ab1f42f0f7b6f257b73afb103ffa68aa04723
Christopher Rush <chris.uclan@gmail.com>
2023-07-13 19:36:42 +0100

    Update pijuice_button_test.py

--- pijuice_button_test.py
+++ pijuice_button_test.py
@@ -8,22 +8,25 @@ while True:
     # Get the status of the buttons
     button_events = pijuice.status.GetButtonEvents()
 
-    def check_button_status(button_name):
-        status = button_events['data'][button_name]
-        if status['SINGLE_PRESS']:
-            print(f"{button_name} button single press")
-            pijuice.status.SetButtonEvent(button_name, 'SINGLE_PRESS', False)  # Reset the event
-        if status['DOUBLE_PRESS']:
-            print(f"{button_name} button double press")
-            pijuice.status.SetButtonEvent(button_name, 'DOUBLE_PRESS', False)  # Reset the event
-        if status['LONG_PRESS']:
-            print(f"{button_name} button long press")
-            pijuice.status.SetButtonEvent(button_name, 'LONG_PRESS', False)  # Reset the event
+    if button_events['error'] == 'NO_ERROR':
+        button_data = button_events['data']
 
-    # Check status of each button
-    check_button_status('SW1')
-    check_button_status('SW2')
-    check_button_status('SW3')
+            def check_button_status(button_name):
+            status = button_data[button_name]
+            if status['SINGLE_PRESS']:
+                print(f"{button_name} button single press")
+                pijuice.status.SetButtonEvent(button_name, 'SINGLE_PRESS', False)  # Reset the event
+            if status['DOUBLE_PRESS']:
+                print(f"{button_name} button double press")
+                pijuice.status.SetButtonEvent(button_name, 'DOUBLE_PRESS', False)  # Reset the event
+            if status['LONG_PRESS']:
+                print(f"{button_name} button long press")
+                pijuice.status.SetButtonEvent(button_name, 'LONG_PRESS', False)  # Reset the event
+
+        # Check status of each button
+        check_button_status('SW1')
+        check_button_status('SW2')
+        check_button_status('SW3')
 
     # Delay to prevent CPU overuse
     time.sleep(0.1)
