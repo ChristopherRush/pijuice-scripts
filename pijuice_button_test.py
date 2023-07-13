7143f1c589366fad0d166a39458e764226968bc2
Christopher Rush <chris.uclan@gmail.com>
2023-07-13 19:46:19 +0100

    Update pijuice_button_test.py

--- pijuice_button_test.py
+++ pijuice_button_test.py
@@ -4,14 +4,21 @@ import time
 # Initialize PiJuice interface
 pijuice = PiJuice(1, 0x14)
 
-def check_button_status(button_name, button_data):
-    status = button_data[button_name]
+def check_button_status(button_events, button_name):
+    if button_events['error'] != 'NO_ERROR':
+        print("Error in getting button status")
+        return
+
+    status = button_events['data'][button_name]
+
     if status['SINGLE_PRESS']:
         print(f"{button_name} button single press")
         pijuice.status.SetButtonEvent(button_name, 'SINGLE_PRESS', False)  # Reset the event
+
     if status['DOUBLE_PRESS']:
         print(f"{button_name} button double press")
         pijuice.status.SetButtonEvent(button_name, 'DOUBLE_PRESS', False)  # Reset the event
+
     if status['LONG_PRESS']:
         print(f"{button_name} button long press")
         pijuice.status.SetButtonEvent(button_name, 'LONG_PRESS', False)  # Reset the event
@@ -20,14 +27,10 @@ while True:
     # Get the status of the buttons
     button_events = pijuice.status.GetButtonEvents()
 
-    # Check if the 'data' key exists and is a dictionary
-    if 'data' in button_events and isinstance(button_events['data'], dict):
-        button_data = button_events['data']
-
-        # Check status of each button
-        check_button_status('SW1', button_data)
-        check_button_status('SW2', button_data)
-        check_button_status('SW3', button_data)
+    # Check status of each button
+    check_button_status(button_events, 'SW1')
+    check_button_status(button_events, 'SW2')
+    check_button_status(button_events, 'SW3')
 
     # Delay to prevent CPU overuse
     time.sleep(0.1)
