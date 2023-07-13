2dcc40531017db05d125d0fd2e88ca0cf12e667f
Christopher Rush <chris.uclan@gmail.com>
2023-07-13 19:54:17 +0100

    Update pijuice_button_test.py

--- pijuice_button_test.py
+++ pijuice_button_test.py
@@ -1,36 +1,28 @@
 from pijuice import PiJuice
-import time
 
 # Initialize PiJuice interface
 pijuice = PiJuice(1, 0x14)
 
-def check_button_status(button_events, button_name):
-    if button_events['error'] != 'NO_ERROR':
-        print("Error in getting button status")
-        return
-
-    status = button_events['data'][button_name]
-
-    if status['SINGLE_PRESS']:
-        print(f"{button_name} button single press")
-        pijuice.status.SetButtonEvent(button_name, 'SINGLE_PRESS', False)  # Reset the event
-
-    if status['DOUBLE_PRESS']:
-        print(f"{button_name} button double press")
-        pijuice.status.SetButtonEvent(button_name, 'DOUBLE_PRESS', False)  # Reset the event
-
-    if status['LONG_PRESS']:
-        print(f"{button_name} button long press")
-        pijuice.status.SetButtonEvent(button_name, 'LONG_PRESS', False)  # Reset the event
-
 while True:
     # Get the status of the buttons
-    button_events = pijuice.status.GetButtonEvents()
+    button_status = pijuice.status.GetButtonStatus()
+
+    # Function to check button status
+    def check_button_status(button_name):
+        status = button_status['data'][button_name]
+        if status == 'SINGLE_PRESS':
+            print(f"{button_name} button single press")
+        elif status == 'DOUBLE_PRESS':
+            print(f"{button_name} button double press")
+        elif status == 'LONG_PRESS':
+            print(f"{button_name} button long press")
+        else:
+            print(f"{button_name} button is not pressed")
 
     # Check status of each button
-    check_button_status(button_events, 'SW1')
-    check_button_status(button_events, 'SW2')
-    check_button_status(button_events, 'SW3')
+    check_button_status('SW1')
+    check_button_status('SW2')
+    check_button_status('SW3')
 
     # Delay to prevent CPU overuse
     time.sleep(0.1)
